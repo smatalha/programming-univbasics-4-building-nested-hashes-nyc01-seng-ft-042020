9bf9e0166d228b42c55681a2e54aa7cc487473a6
Md Abu Talha Mian <sma.talha@yahoo.com>
2020-04-10 10:49:11 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -72,6 +72,7 @@ def monopoly_with_fourth_tier
         :three_piece_owned => 100,
         :four_piece_owned => 200
       },
+      {
       :names => {
         :reading_railroad => {:mortgage_value => 100},
         :pennsylvania_railroad => {:mortgage_value => 200},
