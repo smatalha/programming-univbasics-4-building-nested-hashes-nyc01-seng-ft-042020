a51997a5cbc3200f272b8e967200bf6b0c7bd683
Md Abu Talha Mian <sma.talha@yahoo.com>
2020-04-10 10:51:47 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,7 +1,9 @@
 def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
-
+  {
+    railroads: {}
+  }
 end
 
 def monopoly_with_second_tier
@@ -9,7 +11,16 @@ def monopoly_with_second_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-
+   def base_hash
+  {
+    railroads: {
+      :pieces => 4,
+      :rent_in_dollars => {},
+      :names => {}
+    }
+  }
+ end
+ base_hash
 end
 
 def monopoly_with_third_tier
@@ -17,7 +28,31 @@ def monopoly_with_third_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+  def monopoly_with_second_tier
 
+   def base_hash
+  {
+    railroads: {
+      :pieces => '4',
+      :rent_in_dollars => {
+        :one_piece_owned => 25,
+        :two_pieces_owned => 50,
+        :three_pieces_owned => 100,
+        :four_pieces_owned => 200
+      },
+      :names => {
+        :reading_railroad => {},
+        :pennsylvania_railroad => {},
+        :b_and_o_railroad => {},
+        :shortline_railroad => {}
+      }
+    }
+  }
+
+ end
+   base_hash
+end
+monopoly_with_second_tier
 end
 
 def monopoly_with_fourth_tier
@@ -25,5 +60,31 @@ def monopoly_with_fourth_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+  def monopoly_with_third_tier
+  def monopoly_with_second_tier
+   def base_hash
+  {
+    railroads: {
+      :pieces => '4',
+      :rent_in_dollars => {
+        :one_piece_owned => 25,
+        :two_piece_owned => 50,
+        :three_piece_owned => 100,
+        :four_piece_owned => 200
+      },
+      :names => {
+        :reading_railroad => {:mortgage_value => 100},
+        :pennsylvania_railroad => {:mortgage_value => 200},
+        :b_and_o_railroad => {:mortgage_value => 400},
+        :shortline_railroad => {:mortgage_value => 800}
+      }
+    }
+  }
 
+    end
+   base_hash
+  end
+  monopoly_with_second_tier
+ end
+ monopoly_with_third_tier 
 end
